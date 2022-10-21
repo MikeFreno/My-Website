aa0edfa11463312e3bd5871010b329862d500580
MikeFreno <michaelt.freno@gmail.com>
2022-10-21 08:28:58 -0400

    Navbar Mobile Fix v0.82

--- main.py
+++ main.py
@@ -527,4 +527,4 @@ def send_contact_email(name, email, message):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
