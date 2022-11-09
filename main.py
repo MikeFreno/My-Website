43d0610c564ee871c8d789436734d4a0e34a987d
MikeFreno <michaelt.freno@gmail.com>
2022-11-08 21:05:34 -0500

    Deploy on Google Cloud Run(previously heroku) v0.98

--- main.py
+++ main.py
@@ -701,4 +701,4 @@ def gravatar_gen(email):
     return g.get_image(size=100,default='identicon')
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=False)
