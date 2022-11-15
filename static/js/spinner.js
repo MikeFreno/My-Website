6e18241425630a7b96a26f172a670bc1582ca362
MikeFreno <michaelt.freno@gmail.com>
2022-11-14 21:05:49 -0500

    Loading Spinner v0.99.5

---
+++ static/js/spinner.js
@@ -0,0 +1,4 @@
+function spinnerHandler(){
+  var button = document.getElementById("picture_submit");
+  button.innerHTML = 'Loading...<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" id="submit_span"></span>';
+}
